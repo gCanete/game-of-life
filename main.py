9e05434c9f9c053ab015e5fb856993bcb961e1ae
gCanete <g.canete.riaza@gmail.com>
2025-03-01 14:08:31 -0600

    Added squares and empty spaces to show better the display. The  terminal size is used to generate the matrix size.

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import curses
 import random
 import keyboard
 
-mywindow = curses.initscr()
-
 class GameState(object):
     def __init__(self, dim):
-        self._matrix = [[random.randint(0,1) for x in range(dim)] for y in range(dim)]
+        self._matrix = [[random.randint(0, 1) for x in range(dim)] for y in range(dim)]
         self.dim = dim
 
     @property
@@ -21,12 +19,28 @@ class GameState(object):
         # Check the neighbors to a cell. Maximum it can be 8.
         neighbors = 0
         mat = self._matrix
-        for i in range(row-1, row+2):
-            for j in range(col-1, col+2):
-                if i >= self.dim:
-                    i = 0
-                if j >= self.dim:
-                    j = 0 
+        if row == 0:
+            min_row = 0
+        else:
+            min_row = row - 1
+
+        if row == self.dim - 1:
+            max_row = self.dim - 1
+        else:
+            max_row = row + 1
+
+        if col == 0:
+            min_col = 0
+        else:
+            min_col = col - 1
+
+        if col == self.dim - 1:
+            max_col = self.dim - 1
+        else:
+            max_col = col + 1
+        
+        for i in range(min_row, max_row + 1):
+            for j in range(min_col, max_col + 1):
                 if mat[i][j]:
                     neighbors += mat[i][j]
         return neighbors - mat[row][col]
@@ -40,7 +54,7 @@ class GameState(object):
                 if neighbors < 2 or neighbors > 3:
                     next[i][j] = 0
                 elif neighbors == 3:
-                    next[i][j] = 0
+                    next[i][j] = 1
                 else:
                     next[i][j] = mat[i][j]
         self._matrix = next
@@ -48,22 +62,23 @@ class GameState(object):
     def get_matrix_string(self):
         x = ''
         for row in self._matrix:
-            x += ' '.join(str(item) for item in row)
+            for item in row:
+                if item:
+                    x  += '■' + ' '
+                else:
+                    x += ' ' + ' '
             x += "\n"
         return x
 
 if __name__=="__main__":
-    game_of_life = GameState(15)
-    z = 100
-    while z > 1:
-        # matrix = update_matrix(matrix)
-        # mywindow.addstr(0,0, get_matrix_string(matrix))        
-        # mywindow.addstr(0,0, get_matrix_string(matrix))
-        mywindow.addstr(0,0, game_of_life.get_matrix_string())
-        mywindow.refresh()
-        game_of_life.update_matrix()
-        z -= 1
-        time.sleep(1)
+    mywindow = curses.initscr()
+    height,width = mywindow.getmaxyx()
+    num = min(height,width)
+    game_of_life = GameState(num - 1)
 
-    curses.endwin()
-    quit()
+    while  True:
+        mywindow.refresh()
+        mywindow.addstr(0,0, game_of_life.get_matrix_string())
+        game_of_life.update_matrix()
+        time.sleep(0.1)
+        mywindow.refresh()
